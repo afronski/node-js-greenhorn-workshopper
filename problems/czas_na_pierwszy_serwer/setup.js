fc62d56570dc1bc81c50834e0d68ece9608cde44
Wojciech Gawronski <afronski@gmail.com>
2013-12-24 13:34:52 +0100

    Task with creating TCP time server added.

--- problems/czas_na_pierwszy_serwer/setup.js
+++ problems/czas_na_pierwszy_serwer/setup.js
@@ -1,37 +1,40 @@
-const through    = require('through')
-    , net        = require('net')
-    , bl         = require('bl')
+"use strict";
+
+var net = require("net"),
+
+    through = require("through");
 
 module.exports = function (run) {
-  var outputA = through()
-    , outputB = through()
-    , portA = 1024 + Math.floor(Math.random() * 64511)
-    , portB = portA+1
+    var outputA = through(),
+        outputB = through(),
+        portA = 1024 + Math.floor(Math.random() * 64511),
+        portB = portA + 1;
 
-  function error (url, out, err) {
-    out.write('Error connecting to ' + url + ': ' + err.message)
-    out.end()
-  }
-
-  setTimeout(function () {
-    var hqa
-      , hqb
-
-    hqa = net.connect(portA)
-    hqa.on('error', error.bind(null, 'localhost:' + portA, outputA))
-      .pipe(outputA)
-
-    if (!run) {
-      hqb = net.connect(portB)
-      hqb.on('error', error.bind(null, 'localhost:' + portB, outputB))
-        .pipe(outputB)
+    function error(url, out, err) {
+        out.write("Error connecting to " + url + ": " + err.message);
+        out.end();
     }
-  }, 500)
 
-  return {
-      submissionArgs : [portA]
-    , solutionArgs : [portB]
-    , a    : outputA
-    , b    : outputB
-  }
-}
+    function verification() {
+        var hqa,
+            hqb;
+
+        hqa = net.connect(portA);
+        hqa.on("error", error.bind(null, "localhost:" + portA, outputA)).pipe(outputA);
+
+        if (!run) {
+            hqb = net.connect(portB);
+            hqb.on("error", error.bind(null, "localhost:" + portB, outputB)).pipe(outputB);
+        }
+    }
+
+    setTimeout(verification, 500);
+
+    return {
+        submissionArgs: [ portA ],
+        solutionArgs: [ portB ],
+
+        a: outputA,
+        b: outputB
+    };
+};
--- problems/czas_na_pierwszy_serwer/solution.js
+++ problems/czas_na_pierwszy_serwer/solution.js
@@ -1,19 +1,24 @@
-var net = require('net')
+"use strict";
 
-function zeroFill(i) {
-  return (i < 10 ? '0' : '') + i
+var net = require("net"),
+    os = require("os");
+
+function pad(i) {
+  return (i < 10 ? "0" : "") + i;
 }
 
 function now () {
-  var d = new Date()
-  return d.getFullYear() + '-'
-    + zeroFill(d.getMonth() + 1) + '-'
-    + zeroFill(d.getDate()) + ' '
-    + zeroFill(d.getHours()) + ':'
-    + zeroFill(d.getMinutes())
+  var d = new Date();
+
+  return d.getFullYear() + "-" +
+         pad(d.getMonth() + 1) + "-" +
+         pad(d.getDate()) + " " +
+         pad(d.getHours()) + ":" +
+         pad(d.getMinutes());
 }
 
 var server = net.createServer(function (socket) {
-  socket.end(now() + '\n')
-})
-server.listen(Number(process.argv[2]))
+  socket.end(now() + os.EOL);
+});
+
+server.listen(parseInt(process.argv[2], 10));
